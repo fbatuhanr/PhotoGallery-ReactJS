7b458dbf94bc13dc1d58b1ff0a3d503c475ec05e
Batuhan Ozturk <fbatuhanr@gmail.com>
2021-09-13 00:24:37 +0300

    Photo title input added

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -3,37 +3,67 @@ import ProgressBar from './ProgressBar';
 
 function UploadForm() {
 
-    const [file, setFile] = useState(null);
+    const [startUpload, setStartUpload] = useState(false);
+
+    const [file, setFile] = useState({
+        photo: null,
+        title: null
+    });
     const [error, setError] = useState(null);
 
-    const allowedTypes = ['image/png', 'image/jpeg'];
+    const changeInput = (e) => {
+        setFile({
+            ...file,
+            [e.target.name]: e.target.files ? e.target.files[0] : e.target.value
+        })
+    }
 
-    const uploadChange = (e) => {
-        let selectedFile = e.target.files[0];
+    const allowedTypes = ['image/png', 'image/jpeg'];
+    const formUpload = (e) => {
+        e.preventDefault();
+
+        
+        console.log("1photo:" + file.photo);
+        console.log("1title:" + file.title);
+        
+        let selectedFile = file.photo;
+        let selectedTitle = file.title;
 
         if( selectedFile && allowedTypes.includes(selectedFile.type) ){
-            setFile(selectedFile);
+            
+            setStartUpload(true);
             setError(null);
         }
         else {
-            setFile(null);
+            setStartUpload(false);
             setError("Please select an image file (png or jpeg)!");
         }
+        
     }
 
     return (
         <div className="row justify-content-center mt-3">
-          <div className="col col-6">
-              <form action="" method="post">
-  
-                <label htmlFor="uploadPhoto" className="form-label h4">Upload a photo for start...</label>
-                <input onChange={uploadChange}
-                className="form-control form-control-lg" id="uploadPhoto" type="file" />
+          <div className="col col-8 pt-5 pb-5">
+              <form action="" method="post" onSubmit={formUpload}>
+
+                <div className="input-group input-group-lg">
+                    <label className="input-group-text" htmlFor="photo">Photo:</label>
+                    <input onChange={changeInput} type="file" className="form-control form-control-lg" id="photo" name="photo" required/>
+                </div>
+
+                <div className="input-group input-group-lg mt-3 mb-3">
+                    <span className="input-group-text">Photo Title:</span>
+                    <input onChange={changeInput} type="text" className="form-control" id="title" name="title" required/>
+                </div>
+
+                <div className="d-grid gap-2">
+                    <button type="submit" className="btn btn-secondary">Upload Photo</button>
+                </div>
   
                 <div className="inputOutput">
                     <div className="error"> {error} </div>
 
-                    { file && <ProgressBar file={file} setFile={setFile}/> }
+                    {  startUpload && file && <ProgressBar file={file} setFile={setFile} setStartUpload={setStartUpload}/>  }
                 </div>
   
               </form>
